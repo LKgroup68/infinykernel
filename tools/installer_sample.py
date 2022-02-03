b004bc4e070c75fe1de3b7909270f63dce33769e
LKgroup68 <96568049+LKgroup68@users.noreply.github.com>
2022-02-03 17:53:31 +0100

    Create installer_sample.py

---
+++ tools/installer_sample.py
@@ -0,0 +1,23 @@
+#import the modules
+import os 
+import shutil
+import time 
+import random
+import socket
+import psutil
+#installer
+print("INFINY INSTALLER")
+print("Loading your files.........................................................................................................................")
+#wait for the user can view the installer
+time.sleep(3)
+#start while
+while(1)
+ os.system("git clone https://github.com/LKgroup68/infinykernel/main.py")#add your reposera or a packager manager url and command
+ os.system("clear")
+ print("install is finished, wait 3 second")
+ time.sleep(3)
+ os.system("clear")
+ os.system("python3 main.py")
+  
+ 
+
