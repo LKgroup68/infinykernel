fa175858e215315583ef52354a23019b76d6ae19
LKgroup68 <96568049+LKgroup68@users.noreply.github.com>
2022-02-12 09:04:04 +0100

    Create InfinyUI.py

---
+++ InfinyUI.py
@@ -0,0 +1,9 @@
+#import modules/importation des modules
+import time
+import tkinter
+import os
+#advertisment/avertissement
+print("InfinyUI")
+print("Starting.... (you have 3 second to press CTRL+C for stop UI)")
+time.sleep("3")
+#defined setting/definitions des paramatres
